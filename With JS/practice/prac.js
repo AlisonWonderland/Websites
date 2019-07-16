573beefa87bc154104aec2bc61649f3257ab0151
AlisonWonderland <dimitridawn@gmail.com>
2019-07-16 07:52:42 -0700

    Modal images practice

--- With JS/practice/prac.js
+++ With JS/practice/prac.js
@@ -1,3 +1,17 @@
-var age = prompt("What is your age");
+function kebabToSnake(kebab) {
+    snake = "";
+    lastIndex = 0;
+    for(var i = 0; i < kebab.length; ++i) {
+        if(kebab.charAt(i) === "-") {
+            snake += kebab.slice(lastIndex, i) + "_"
+            lastIndex = i+1;
+        }
+    }
+    snake += kebab.slice(lastIndex, kebab.length-lastIndex);
+    return snake;
+}
 
-console.log(age + " years is roughly " + age * 365 + " days");
+while(1){
+    var response = prompt("Enter kebab case string");
+    alert(kebabToSnake(response));
+}
