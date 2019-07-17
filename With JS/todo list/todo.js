7a54f76729af6aa03c9d6645b42df1f057a714e9
AlisonWonderland <dimitridawn@gmail.com>
2019-07-17 14:56:04 -0700

    Added delete feature

--- With JS/todo list/todo.js
+++ With JS/todo list/todo.js
@@ -1,21 +1,60 @@
+window.setTimeout(function() {
+    var todoList = [];
 
-
-
-var response = "";
-
-do {
-    response = prompt("What would you like to do?");
-
-    if(response == "new"){
-        newTodo();
+    function newTodo() {
+        var todo = prompt("Enter a new todo");
+        todoList.push(todo);
     }
-    else if(response == "list"){
-        printTodos();
+
+    function printTodos() {
+        if(todoList.length == 0) {
+            console.log("No todos in list");
+            return;
+        }
+
+        console.log("********");
+        todoList.forEach(function(todo, index) {
+            console.log(index + ': ' + todo);
+        });
+        console.log("********");
     }
-    else if(response == "quit"){
-        break;
+
+    function deleteTodo() {
+        index = prompt("Enter index of todo to delete");
+
+        if(todoList.length === 0) {
+            console.log("Can't. No items in list.");
+            return;
+        }
+
+        else if(index < 0 || index >= todoList.length) {
+            console.log("Index is invalid");
+            return;
+        }
+
+        else {
+            todoList.splice(index,1);
+        }
+
+        console.log("Todo Removed");
     }
-    else {
-        alert("Invalid");
-    }
-}while(response !== "quit")
+
+    var response = "";
+
+    do {
+        response = prompt("What would you like to do?");
+
+        if(response == "new"){
+            newTodo();
+        }
+        else if(response == "list"){
+            printTodos();
+        }
+        else if(response == "delete"){
+            deleteTodo();
+        }
+        else {
+            alert("Invalid");
+        }
+    }while(response !== "quit")
+}, 500);
