abae73c2b010813158541a13b479fc84337ca427
AlisonWonderland <dimitridawn@gmail.com>
2019-08-02 19:45:43 -0700

    Finished website

---
+++ Todo List v2/assets/js/todos.js
@@ -0,0 +1,41 @@
+
+
+
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("check-off");
+});
+
+$("ul").on("click", "span.trash", function() {
+    $(this).parent().fadeOut(400, function() {
+        $(this).remove();
+    });
+});
+
+$("input[type=text]").keypress(function(event) {
+    user_input = removeFrontSpaces($(this).val());
+    //add new nonempty todo after enter is pressed
+    if(event.which === 13 && user_input != '') {
+        $("ul").append('<li> <span class="trash"><i class="fas fa-trash"></i></span> ' +  user_input + '</li>');
+        $(this).val("");
+    }
+});
+
+$(".fa-plus").click(function() {
+    $("input[type=text]").fadeToggle();
+});
+
+function removeFrontSpaces(user_todo) {
+    //assume there are no spaces in the front
+    var last_space_index = -1;
+
+    for(var i = 0; i < user_todo.length; ++i) {
+        if(user_todo[i] === ' ') {
+            last_space_index = i;
+        }
+        else {
+            break;
+        }
+    }
+
+    return user_todo.slice(last_space_index + 1);
+}
